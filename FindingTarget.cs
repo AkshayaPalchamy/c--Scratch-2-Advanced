25a213c5c6ef6a7333d9eef2a8f46b6674a097a4
Akshaya <akshaya@astravor.com>
2026-02-23 14:40:16 +0530

    ADDED: FindingTargets problem

---
+++ FindingTarget.cs
@@ -0,0 +1,31 @@
+﻿public class FindingTargets
+{ 
+	public static void Main(string[] args)
+	{
+		Console.WriteLine("Enter the length of an array:");
+		bool nArray2 = int.TryParse(Console.ReadLine(), out int nValue);
+
+		int[] nArray = new int[nValue];
+
+		Console.WriteLine("Enter numbers:");
+		for(int i = 0; i < nValue; i++)
+		{
+			int.TryParse(Console.ReadLine(), out nArray[i]);
+		}
+
+		Console.WriteLine("Enter target");
+		int.TryParse(Console.ReadLine(), out int target);
+
+		for(int i = 0; i < nArray.Length; i++)
+		{
+			for(int j = i+1; j < nArray.Length; j++)
+			{
+				if(nArray[i] + nArray[j] == target)
+				{
+					Console.WriteLine($"{i},{j}");
+					return;
+				}
+			}
+		}
+	}
+}
